07d297ca56d727617cb85df777e8722fd543cf48
VIPIN-PASWAN <vipin1911016@akgec.ac.in>
2020-11-13 02:08:48 +0530

    Setting Report position by name and place value

--- EscapingBuilding/Source/EscapingBuilding/PositionReport.cpp
+++ EscapingBuilding/Source/EscapingBuilding/PositionReport.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "GameFramework/Actor.h"
 #include "PositionReport.h"
 
 // Sets default values for this component's properties
@@ -19,7 +19,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at this %s\n"),*ObjectName,*ObjectPos);
 	
 }
 
